ccdb3d3aaa83b1d9dba5cc5e733a110b81c71135
xiplus <huangxuanyuxiplus@gmail.com>
2018-01-25 22:59:43 +0800

    refactoring, add the day text, add test mode

--- config_example.php
+++ config_example.php
@@ -1,7 +1,22 @@
 <?php
-$config['page_id']='800668270014037';
-$config['exam_list']=array(
-	array("text"=>"學測 還有 \D 天 \W 週","year"=>2016,"month"=>1,"date"=>22),
-	array("text"=>"指考 還有 \D 天 \W 週","year"=>2016,"month"=>7,"date"=>1)
-);
-?>
+
+$C['FBpageid'] = 'page_id';
+$C['FBpagetoken'] = 'page_token';
+
+$C['exam_list'] = [
+	[
+		"text" => "107學測 還有 \D 天 (\W 週)",
+		"year" => 2018,
+		"month" => 1,
+		"date" => 26,
+		"len" => 2
+	],
+	[
+		"text" => "107指考 還有 \D 天 (\W 週)",
+		"year" => 2018,
+		"month" => 7,
+		"date" => 1
+	]
+];
+
+$C["allowsapi"] = array("cli");
--- exam.php
+++ exam.php
@@ -1,45 +1,59 @@
 <?php
-if(PHP_SAPI!="cli"){
-	echo "No permission.";
-	exit;
+require(__DIR__.'/config.php');
+if (!in_array(PHP_SAPI, $C["allowsapi"])) {
+	exit("No permission");
 }
-require_once(__DIR__.'/../global_config.php');
-require_once(__DIR__.'/config.php');
-require_once($config['sql_path']);
-require_once($config['facebook_sdk_path']);
 date_default_timezone_set("Asia/Taipei");
 
-$fb = new Facebook\Facebook([
-	'app_id' => $config['app_id'],
-	'app_secret' => $config['app_secret'],
-	'default_access_token' => $config['access_token'],
-	'default_graph_version' => 'v2.5',
-]);
-$response = $fb->get('/me/accounts')->getDecodedBody();
-foreach($response["data"] as $temp){
-	if($temp["id"]==$config['page_id']){
-		$page_token=$temp["access_token"];
-		break;
-	}
+$testmode = isset($argv[1]);
+$year = date("Y");
+$month = date("n");
+$date = date("j");
+if (isset($argv[3]) && is_numeric($argv[1]) && is_numeric($argv[2]) && is_numeric($argv[3])) {
+	$year = $argv[1];
+	$month = $argv[2];
+	$date = $argv[3];
 }
+echo "post as ".$year."/".$month."/".$date."\n";
 
-$message="";
-foreach($config['exam_list'] as $temp){
-	$today = mktime(0,0,0);
-	$tdehu = mktime(0,0,0,$temp["month"],$temp["date"],$temp["year"]);
+$message = "";
+foreach ($C['exam_list'] as $temp) {
+	$today = mktime(0, 0, 0, $month, $date, $year);
+	$tdehu = mktime(0, 0, 0, $temp["month"], $temp["date"], $temp["year"]);
 	$diff = ($tdehu - $today) / 86400;
-	if($diff>0){
-		$temp["text"]=preg_replace("/\\\D/", $diff, $temp["text"]);
-		$temp["text"]=preg_replace("/\\\W/", floor($diff/7), $temp["text"]);
-		$message.=$temp["text"]."\n";
+	if ($diff > 0) {
+		$temp["text"] = preg_replace("/\\\D/", $diff, $temp["text"]);
+		$temp["text"] = preg_replace("/\\\W/", floor($diff/7), $temp["text"]);
+		$message .= $temp["text"]."\n";
+	}
+	if (isset($temp["len"]) && $diff <= 0 && $diff > -$temp["len"]) {
+		$message .= $temp["thedaytext"]."\n";
 	}
 }
-$message.="\n".date("Y/m/d");
+$message .= "\n".date("Y/m/d");
+
+echo "message:\n";
 echo $message."\n";
 
-$params = array(
-	"message"=>$message
+if ($testmode) {
+	exit("test mode on\n");
+}
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://graph.facebook.com/v2.8/me/feed");
+curl_setopt($ch, CURLOPT_POST, true);
+$post = array(
+	"message" => $message,
+	"access_token" => $C['FBpagetoken']
 );
-$response=$fb->post("/".$config['page_id']."/feed",$params,$page_token)->getDecodedBody();
-var_dump($response);
-?>
+curl_setopt($ch,CURLOPT_POSTFIELDS, http_build_query($post));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+$res = curl_exec($ch);
+curl_close($ch);
+
+$res = json_decode($res, true);
+if (isset($res["error"])) {
+	echo json_encode($res)."\n";
+} else {
+	echo "Success\n";
+}

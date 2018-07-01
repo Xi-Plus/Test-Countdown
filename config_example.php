2bfc9ac4687ea1fc08f05220cc4e9de4604eee02
Xi-Plus <huangxuanyuxiplus@gmail.com>
2018-07-01 23:54:55 +0800

    Fix thedaytext. Support start-post-date

--- config_example.php
+++ config_example.php
@@ -5,18 +5,20 @@ $C['FBpagetoken'] = 'page_token';
 
 $C['exam_list'] = [
 	[
-		"text" => "107學測 還有 \D 天 (\W 週)",
-		"year" => 2018,
-		"month" => 1,
-		"date" => 26,
-		"len" => 2
+		"text" => "107指考 還有 \D 天 (\W 週)",
+		"textwithoutweek" => "107指考 還有 \D 天",
+		"thedaytext" => "今天是107指考，祝各位考生考試順利～",
+		"date_start" => strtotime("2018/7/1"),
+		"date_end" => strtotime("2018/7/3")
 	],
 	[
-		"text" => "107指考 還有 \D 天 (\W 週)",
-		"year" => 2018,
-		"month" => 7,
-		"date" => 1
-	]
+		"text" => "108學測(預計) 還有 \D 天 (\W 週)",
+		"textwithoutweek" => "108學測(預計) 還有 \D 天",
+		"thedaytext" => "今天是108學測，祝各位考生考試順利～",
+		"date_start" => strtotime("2019/1/25"),
+		"date_end" => strtotime("2019/1/26"),
+		"start_post_date" => strtotime("2018/7/4")
+	],
 ];
 
 $C["allowsapi"] = array("cli");
--- exam.php
+++ exam.php
@@ -1,36 +1,43 @@
 <?php
+date_default_timezone_set("Asia/Taipei");
 require(__DIR__.'/config.php');
 if (!in_array(PHP_SAPI, $C["allowsapi"])) {
 	exit("No permission");
 }
-date_default_timezone_set("Asia/Taipei");
 
 $testmode = isset($argv[1]);
-$year = date("Y");
-$month = date("n");
-$date = date("j");
-if (isset($argv[3]) && is_numeric($argv[1]) && is_numeric($argv[2]) && is_numeric($argv[3])) {
-	$year = $argv[1];
-	$month = $argv[2];
-	$date = $argv[3];
+$today = time();
+if (isset($argv[1])) {
+	$today = strtotime($argv[1]);
 }
-echo "post as ".$year."/".$month."/".$date."\n";
+echo "post as ".date("Y/m/d", $today)."\n";
 
 $message = "";
 foreach ($C['exam_list'] as $temp) {
-	$today = mktime(0, 0, 0, $month, $date, $year);
-	$tdehu = mktime(0, 0, 0, $temp["month"], $temp["date"], $temp["year"]);
+	if (isset($temp["start_post_date"]) && $today < $temp["start_post_date"]) {
+		continue;
+	}
+	$tdehu = $temp["date_start"];
 	$diff = ($tdehu - $today) / 86400;
 	if ($diff > 0) {
-		$temp["text"] = preg_replace("/\\\D/", $diff, $temp["text"]);
-		$temp["text"] = preg_replace("/\\\W/", floor($diff/7), $temp["text"]);
-		$message .= $temp["text"]."\n";
-	}
-	if (isset($temp["len"]) && $diff <= 0 && $diff > -$temp["len"]) {
-		$message .= $temp["thedaytext"]."\n";
+		if (floor($diff/7) == 0) {
+			$temp["textwithoutweek"] = preg_replace("/\\\D/", $diff, $temp["textwithoutweek"]);
+			$message .= $temp["textwithoutweek"]."\n";
+		} else {
+			$temp["text"] = preg_replace("/\\\D/", $diff, $temp["text"]);
+			$temp["text"] = preg_replace("/\\\W/", floor($diff/7), $temp["text"]);
+			$message .= $temp["text"]."\n";
+		}
+	} else if (isset($temp["date_end"]) && $today <= $temp["date_end"]) {
+		if (isset($temp["thedaytext"])) {
+			$message .= $temp["thedaytext"]."\n";
+		}
 	}
 }
-$message .= "\n".date("Y/m/d");
+if ($message === "") {
+	exit("nothing to post\n");
+}
+$message .= "\n".date("Y/m/d", $today);
 
 echo "message:\n";
 echo $message."\n";
